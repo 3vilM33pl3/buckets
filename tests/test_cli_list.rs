2cdad6361a1cd170fb6c734e7d1f2a92a2fc459a
3vilM33pl3 <ovanac01@mail.bbk.ac.uk>
2025-08-26 14:26:39 +0100

    Setting up bucket repo for integration tests

--- tests/test_cli_list.rs
+++ tests/test_cli_list.rs
@@ -2,6 +2,8 @@ mod common;
 
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
+
     use crate::common::tests::get_test_dir;
     use serial_test::serial;
 
@@ -15,11 +17,31 @@ mod tests {
     #[test]
     #[serial]
     fn test_cli_list() {
-        let temp_dir = get_test_dir();
+        let temp_dir = setup();
         let mut cmd = assert_cmd::Command::cargo_bin("buckets").expect("failed to run command");
         cmd.current_dir(temp_dir.as_path())
             .arg("list")
             .assert()
             .success();
+    }
+
+    fn setup() -> PathBuf {
+        let temp_dir = get_test_dir();
+        let mut cmd1 = assert_cmd::Command::cargo_bin("buckets").expect("failed to run command");
+        cmd1.current_dir(temp_dir.as_path())
+            .arg("init")
+            .arg("test_repo")
+            .assert()
+            .success();
+
+        let mut cmd2 = assert_cmd::Command::cargo_bin("buckets").expect("failed to run command");
+        let repo_dir = temp_dir.as_path().join("test_repo");
+        cmd2.current_dir(repo_dir.as_path())
+            .arg("create")
+            .arg("test_bucket")
+            .assert()
+            .success();
+
+        repo_dir
     }
 }
